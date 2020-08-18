66e2f29410757585a76563de9f6458ecd42005e8
p.franek <p.franek@kendaxa.com>
2020-08-19 01:21:20 +0200

    exp

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "Intended Audience :: Developers",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python:: 3.8",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
     ],
